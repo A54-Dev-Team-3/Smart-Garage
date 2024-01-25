bc9b4e5f458228811dc12ce27cecd69f4a81d8da
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-25 13:22:24 +0200

    Added UserRepository Interface

---
+++ Smart-Garage/Repositories/Contracts/IUsersRepository.cs
@@ -0,0 +1,18 @@
+﻿using Smart_Garage.Models;
+
+namespace Smart_Garage.Repositories.Contracts
+{
+    public interface IUsersRepository
+    {
+        IList<User> GetAll();
+        //IList<User> FilterBy(UserQueryParameters usersParams);
+        User GetById(int id);
+        User GetByName(string name);
+        User Create(User newUser); // Register
+        User Update(int id, User user);
+        User Delete(int id);
+        User Block(int id);
+        bool UserExists(string name);
+        int Count();
+    }
+}
