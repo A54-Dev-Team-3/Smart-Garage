86701e7081784ecef6d068b49342d308632364f9
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-26 11:26:03 +0200

    Added IServiceRepository

---
+++ Smart-Garage/Repositories/Contracts/IServiceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Smart_Garage.Models;
+using Smart_Garage.Repositories.QueryParameters;
+
+namespace Smart_Garage.Repositories.Contracts
+{
+    public interface IServiceRepository
+    {
+        IList<Service> GetAll();
+        Service GetById(int id);
+        Service GetByName(string name);
+        Service Create(Service newService);
+        Service Update(int id, Service updatedService);
+        Service Delete(int id);
+        IList<Service> FilterBy(ServicesQueryParameters usersParams);
+        bool ServiceExists(string name);
+        int Count();
+    }
+}
