794f26f7c04c78cbed38fdb55512b63f4a63600e
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-26 11:26:18 +0200

    Added UserQueryParamaters

---
+++ Smart-Garage/Repositories/QueryParameters/UserQueryParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace Smart_Garage.Repositories.QueryParameters
+{
+    public class UserQueryParameters
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
