0ac172e74bcafff90dd61ceb5f1c563dc4b452f2
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-26 16:45:02 +0200

    Added UserResponseDTO and UserRequestDTO

---
+++ Smart-Garage/Models/DTOs/RequestDTOs/UserRequestDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.RequestDTOs
+{
+    public class UserRequestDTO
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(20, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string Username { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        public string PhoneNumber { get; set; }
+    }
+}
