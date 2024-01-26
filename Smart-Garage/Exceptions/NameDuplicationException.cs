454237c51b0fa539c0195e8667bb34ba81eb5494
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-26 16:40:29 +0200

    Added "NameDuplicationException"

---
+++ Smart-Garage/Exceptions/NameDuplicationException.cs
@@ -0,0 +1,11 @@
+﻿namespace Smart_Garage.Exceptions
+{
+    public class NameDuplicationException : ApplicationException
+    {
+        public NameDuplicationException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
