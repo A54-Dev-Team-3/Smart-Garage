83b271ec8bf7fa76ebedfc30380a3eda3cb4ff89
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-26 16:55:41 +0200

    Added "WrongPasswordException"

---
+++ Smart-Garage/Exceptions/WrongPasswordException.cs
@@ -0,0 +1,11 @@
+﻿namespace Smart_Garage.Exceptions
+{
+    public class WrongPasswordException : ApplicationException
+    {
+        public WrongPasswordException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
