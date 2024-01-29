f390dbbd97d3a0294b7ceac375333226b4aacc2b
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-29 18:58:45 +0200

    Added LoginUserRequestDTO

---
+++ Smart-Garage/Models/DTOs/RequestDTOs/LoginUserRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.RequestDTOs
+{
+    public class LoginUserRequestDTO
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
