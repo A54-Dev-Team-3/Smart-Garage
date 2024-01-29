481fa144d50b934560ba48d67eba960d9c12b14b
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-29 19:00:52 +0200

    Added SignUpRequestDTO

---
+++ Smart-Garage/Models/DTOs/RequestDTOs/SignUpUserRequestDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.RequestDTOs
+{
+    public class SignUpUserRequestDTO
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(20, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        public string Username { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[+*^$%&!@#_-])[A-Za-z\d+*^$%&!@#_-]{8,}$")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        public string PhoneNumber { get; set; }
+    }
+}
