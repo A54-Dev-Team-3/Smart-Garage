6c3968c21d7a35cbfed151e844069f2c5bd9f251
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-29 19:02:50 +0200

    Filled "UserResponseDTO"

--- Smart-Garage/Models/DTOs/ResponseDTOs/UserResponseDTO.cs
+++ Smart-Garage/Models/DTOs/ResponseDTOs/UserResponseDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace Smart_Garage.Models.DTOs.ResponseDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.ResponseDTOs
 {
     public class UserResponseDTO
     {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
