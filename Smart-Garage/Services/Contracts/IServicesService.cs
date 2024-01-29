8cf4461690defb6455aeb04b3a7a933e3cf35739
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-29 11:48:18 +0200

    Filled IServicesService and ServicesService

--- Smart-Garage/Services/Contracts/IServicesService.cs
+++ Smart-Garage/Services/Contracts/IServicesService.cs
@@ -1,7 +1,18 @@
-﻿namespace Smart_Garage.Services.Contracts
+﻿using Smart_Garage.Models.QueryParameters;
+using Smart_Garage.Models;
+
+namespace Smart_Garage.Services.Contracts
 {
     public interface IServicesService
     {
-
+        IList<Service> GetAll();
+        Service GetById(int id);
+        Service GetByName(string username);
+        Service Create(Service newService, string username);
+        Service Update(int id, Service updatedService);
+        Service Delete(int id, string username);
+        IList<Service> FilterBy(ServicesQueryParameters usersParams);
+        bool ServiceExists(string name);
+        int Count();
     }
 }
