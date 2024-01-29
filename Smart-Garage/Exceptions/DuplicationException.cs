71e92b7d5aa469e8533ebf81057c298613ed8ff8
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-29 11:44:54 +0200

    Added DuplicationException

---
+++ Smart-Garage/Exceptions/DuplicationException.cs
@@ -0,0 +1,11 @@
+﻿namespace Smart_Garage.Exceptions
+{
+    public class DuplicationException : ApplicationException
+    {
+        public DuplicationException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
