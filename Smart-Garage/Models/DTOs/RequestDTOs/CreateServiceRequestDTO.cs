77c0aeeb96e7290346cc8a141c1817a6e7fc31f2
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-30 21:30:35 +0200

    Added CreateServiceRequestDTO

---
+++ Smart-Garage/Models/DTOs/RequestDTOs/CreateServiceRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.RequestDTOs
+{
+    public class CreateServiceRequestDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Price { get; set; }
+
+        [Required]
+        public int VehicleId { get; set; }
+    }
+}
