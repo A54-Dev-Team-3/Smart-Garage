34cca4e97d08a2a54ba1f9bb907c10df6e50bf02
Hristian <hristian.kr.nikolov@gmail.com>
2024-01-30 21:37:31 +0200

    Changed the return type and some of the parameters of Create, Update and Delete in IServicesService and ServicesService

--- Smart-Garage/Services/Contracts/IServicesService.cs
+++ Smart-Garage/Services/Contracts/IServicesService.cs
@@ -1,5 +1,7 @@
 ﻿using Smart_Garage.Models.QueryParameters;
 using Smart_Garage.Models;
+using Smart_Garage.Models.DTOs.RequestDTOs;
+using Smart_Garage.Models.DTOs.ResponseDTOs;
 
 namespace Smart_Garage.Services.Contracts
 {
@@ -8,10 +10,10 @@ namespace Smart_Garage.Services.Contracts
         IList<Service> GetAll();
         Service GetById(int id);
         Service GetByName(string username);
-        Service Create(Service newService, string username);
-        Service Update(int id, Service updatedService);
-        Service Delete(int id, string username);
-        IList<Service> FilterBy(ServicesQueryParameters usersParams);
+        CreateServiceResponseDTO Create(CreateServiceRequestDTO newService);
+        UpdateServiceResponseDTO Update(int id, UpdateServiceRequestDTO updatedService, string? username);
+        DeleteServiceResponseDTO Delete(int id);
+        IList<Service> FilterBy(ServicesQueryParameters filterParameters);
         bool ServiceExists(string name);
         int Count();
     }
--- Smart-Garage/Services/ServicesService.cs
+++ Smart-Garage/Services/ServicesService.cs
@@ -1,5 +1,9 @@
-﻿using Smart_Garage.Exceptions;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Smart_Garage.Exceptions;
 using Smart_Garage.Models;
+using Smart_Garage.Models.DTOs.RequestDTOs;
+using Smart_Garage.Models.DTOs.ResponseDTOs;
 using Smart_Garage.Models.QueryParameters;
 using Smart_Garage.Repositories;
 using Smart_Garage.Repositories.Contracts;
@@ -11,10 +15,13 @@ namespace Smart_Garage.Services
     {
         private readonly IServiceRepository servicesRepository;
         private readonly IUsersRepository usersRepository;
+        private readonly IMapper autoMapper;
 
-        public ServicesService(IServiceRepository servicesRepository)
+        public ServicesService(IServiceRepository servicesRepository, IUsersRepository usersRepository, IMapper autoMapper)
         {
             this.servicesRepository = servicesRepository;
+            this.usersRepository = usersRepository;
+            this.autoMapper = autoMapper;
         }
 
         public IList<Service> GetAll()
@@ -32,38 +39,49 @@ namespace Smart_Garage.Services
             return servicesRepository.GetByName(username);
         }
 
-        public Service Create(Service newService, string username)
+        public CreateServiceResponseDTO Create(CreateServiceRequestDTO newService)
         {
-            User user = usersRepository.GetByName(username);
-
-            if (!user.IsAdmin)
-                throw new UnauthorizedOperationException("User is not authorized.");
 
             if (servicesRepository.ServiceExists(newService.Name))
-                throw new UnauthorizedOperationException($"Service with name {newService.Name} already exists.");
+                throw new DuplicationException($"Service with name {newService.Name} already exists.");
 
-            return servicesRepository.Create(newService);
+            Service service = new Service()
+            {
+                Name = newService.Name,
+                Price = newService.Price,
+                VehicleId = newService.VehicleId
+            };
+
+            return autoMapper.Map<CreateServiceResponseDTO>(servicesRepository.Create(service));
         }
 
-        public Service Update(int id, Service updatedService)
+        public UpdateServiceResponseDTO Update(int id, UpdateServiceRequestDTO updatedService, string? username)
         {
-            return servicesRepository.Update(id, updatedService);
+            if (username?.Length == 5)
+            {
+                throw new NotImplementedException("");
+            }
+
+            Service service = new Service()
+            {
+                Name = updatedService.Name,
+                Price = updatedService.Price,
+                VehicleId = updatedService.VehicleId
+            };
+
+            return autoMapper.Map<UpdateServiceResponseDTO>(servicesRepository.Update(id, service));
         }
 
-        public Service Delete(int id, string username)
+        public DeleteServiceResponseDTO Delete(int id)
         {
-            User user = usersRepository.GetByName(username);
-
-            if (!user.IsAdmin)
-                throw new UnauthorizedOperationException($"User is not authorized.");
-
-            return servicesRepository.Delete(id);
+            return autoMapper.Map<DeleteServiceResponseDTO>(servicesRepository.Delete(id));
         }
 
-        public IList<Service> FilterBy(ServicesQueryParameters usersParams)
+        public IList<Service> FilterBy(ServicesQueryParameters filterParameters)
         {
-            // TODO
-            throw new NotImplementedException();
+            return servicesRepository.FilterBy(filterParameters)
+                            .Select(u => autoMapper.Map<Service>(u))
+                            .ToList();
         }
 
         public bool ServiceExists(string name)
