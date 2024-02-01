130950f99524693d332f99886b6fe866b34c2431
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2024-02-01 17:32:33 +0200

    Added new DTOs

---
+++ Smart-Garage/Models/DTOs/RequestDTOs/UserRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Smart_Garage.Models.DTOs.RequestDTOs
+{
+    public class UserRequestDTO
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
