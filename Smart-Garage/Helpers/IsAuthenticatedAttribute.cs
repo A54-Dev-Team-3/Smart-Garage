f4de91d737f3401fec77f25180afcc6a59e9d9c3
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-01 20:10:02 +0200

    Added IsAuthenticatedAttribute

---
+++ Smart-Garage/Helpers/IsAuthenticatedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Smart_Garage.Helpers
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public class IsAuthenticatedAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var isLogged = context.HttpContext.Session.Keys.Contains("user");
+
+            if (!isLogged)
+            {
+                context.Result = new RedirectToRouteResult(new { controller = "Users", action = "Login" });
+            }
+        }
+    }
+}
