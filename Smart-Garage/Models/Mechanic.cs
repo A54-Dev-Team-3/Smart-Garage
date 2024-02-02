b28fed70e0eccc9779130a7630dd4524634a19ef
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-02 20:49:11 +0200

    Added Mechanic

---
+++ Smart-Garage/Models/Mechanic.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models
+{
+    public class Mechanic
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public IList<ServiceInstanceMechanic> ServiceInstanceMechanic { get; set; } = new List<ServiceInstanceMechanic>();
+    }
+}
