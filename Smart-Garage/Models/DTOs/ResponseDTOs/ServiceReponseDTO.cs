00b22cde5c14e5316b882d325b14c956e3d7eb55
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-02 20:48:03 +0200

    Added ServiceReponseDTO

---
+++ Smart-Garage/Models/DTOs/ResponseDTOs/ServiceReponseDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.ResponseDTOs
+{
+    public class ServiceReponseDTO
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
