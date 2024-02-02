4d9cca0139149056e1823889d5c22543b10247e2
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-02 20:47:26 +0200

    Added MechanicRequestDTO

---
+++ Smart-Garage/Models/DTOs/RequestDTOs/MechanicRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Smart_Garage.Models.DTOs.RequestDTOs
+{
+    public class MechanicRequestDTO
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
