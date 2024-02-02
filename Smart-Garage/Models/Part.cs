5935048005b359a799904647d5f4dea9e8eea627
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-02 20:49:17 +0200

    Added Part

---
+++ Smart-Garage/Models/Part.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models
+{
+    public class Part
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public double Price { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        // Image ?
+        public IList<ServiceInstancePart> ServiceInstanceParts { get; set; } = new List<ServiceInstancePart>();
+    }
+}
