091cde65ac572e51061c4fd75f6cffa39965c1f4
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-04 14:22:52 +0200

    Added PartResponseDTO

---
+++ Smart-Garage/Models/DTOs/ResponseDTOs/PartResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Garage.Models.DTOs.ResponseDTOs
+{
+    public class PartResponseDTO
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
