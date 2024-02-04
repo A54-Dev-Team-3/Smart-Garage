ecf1d1d7f4425e9746fef147918d759476fe6314
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-04 14:24:17 +0200

    Added PartRepository

---
+++ Smart-Garage/Repositories/Contracts/IPartRepository.cs
@@ -0,0 +1,19 @@
+﻿using Smart_Garage.Models;
+using Smart_Garage.Models.DTOs.RequestDTOs;
+using Smart_Garage.Models.DTOs.ResponseDTOs;
+using Smart_Garage.Models.QueryParameters;
+
+namespace Smart_Garage.Repositories.Contracts
+{
+    public interface IPartRepository
+    {
+        Part Create(Part newPartDTO);
+        IList<Part> GetAll();
+        IList<Part> FilterBy(PartQueryParameters filterParameters);
+        Part GetById(int id);
+        Part GetByName(string name);
+        Part Update(int id, Part newPartDTO);
+        Part Delete(int id);
+        bool PartExists(string name);
+    }
+}
---
+++ Smart-Garage/Repositories/PartRepository.cs
@@ -0,0 +1,73 @@
+﻿using Smart_Garage.Exceptions;
+using Smart_Garage.Models;
+using Smart_Garage.Models.QueryParameters;
+using Smart_Garage.Repositories.Contracts;
+using System.Xml.Linq;
+
+namespace Smart_Garage.Repositories
+{
+    public class PartRepository : IPartRepository
+    {
+        private readonly SGContext context;
+
+        public PartRepository(SGContext context)
+        {
+            this.context = context;
+        }
+
+        public Part Create(Part newPartDTO)
+        {
+            context.Parts.Add(newPartDTO);
+            context.SaveChanges();
+            return newPartDTO;
+        }
+
+        public IList<Part> GetAll()
+        {
+            return context.Parts.ToList();
+        }
+
+        public IList<Part> FilterBy(PartQueryParameters filterParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Part GetById(int id)
+        {
+            return context.Parts
+                .FirstOrDefault(p => p.Id == id && !p.IsDeleted) ??
+                throw new EntityNotFoundException($"Part with id:{id} not found.");
+        }
+
+        public Part GetByName(string name)
+        {
+            return context.Parts.FirstOrDefault(p => p.Name == name && !p.IsDeleted) ??
+               throw new EntityNotFoundException($"Part with name:{name} is not found.");
+        }
+
+        public Part Update(int id, Part newPartDTO)
+        {
+            Part newPart = context.Parts.FirstOrDefault(s => s.Id == id && !s.IsDeleted) ??
+                throw new EntityNotFoundException($"Parts to update with id:{id} not found.");
+
+            newPart.Name = newPartDTO.Name;
+            newPart.Price = newPartDTO.Price;
+
+            context.SaveChanges();
+            return newPart;
+        }
+
+        public Part Delete(int id)
+        {
+            Part toDelete = GetById(id);
+            toDelete.IsDeleted = true;
+            context.SaveChanges();
+            return toDelete;
+        }
+
+        public bool PartExists(string name)
+        {
+            return context.Parts.Any(p => p.Name == name);
+        }
+    }
+}
