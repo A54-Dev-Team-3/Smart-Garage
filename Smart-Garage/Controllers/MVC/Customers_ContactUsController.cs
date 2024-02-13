a26a50f619adeef49c30dff6f395c600db5c0221
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-13 15:12:27 +0200

    Added Customers_ContactUsController

---
+++ Smart-Garage/Controllers/MVC/Customers_ContactUsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smart_Garage.Helpers;
+
+namespace Smart_Garage.Controllers.MVC
+{
+    public class Customers_ContactUsController : Controller
+    {
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
