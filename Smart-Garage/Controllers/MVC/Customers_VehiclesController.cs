8b1ac6d29086ed89536cc19edd4fb1ae7ef8cacf
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-13 15:13:02 +0200

    Addeed Customers_VehiclesController

---
+++ Smart-Garage/Controllers/MVC/Customers_VehiclesController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Smart_Garage.Helpers;
+using Smart_Garage.Models.DTOs.ResponseDTOs;
+using Smart_Garage.Models.QueryParameters;
+using Smart_Garage.Models.ViewModel;
+using Smart_Garage.Repositories.QueryParameters;
+using Smart_Garage.Services;
+using Smart_Garage.Services.Contracts;
+
+namespace Smart_Garage.Controllers.MVC
+{
+    public class Customers_VehiclesController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IMapper autoMapper;
+        private readonly IVehicleService vehicleService;
+        public Customers_VehiclesController(IUserService userService, IMapper autoMapper, IVehicleService vehicleService)
+        {
+            this.userService = userService;
+            this.autoMapper = autoMapper;
+            this.vehicleService = vehicleService;
+        }
+
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Index()
+        {
+            string username = HttpContext.Session.GetString("user");
+            var userVehiclesResponseDTO = userService.GetByName(username).Vehicles;
+            var vehicleViewModel = autoMapper.Map<IList<VehicleViewModel>>(userVehiclesResponseDTO);
+            
+            return View(vehicleViewModel);
+        }
+
+        [HttpPost]
+        [IsAuthenticated]
+        public IActionResult Index(string id)
+        {
+            return RedirectToAction("Detail", "Customers_Vehicles", new { newId = id });
+        }
+
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Detail(int newId)
+        {
+            return View();
+        }
+    }
+}
