2716a4829b8d62fc03209683c42159bef7b18a98
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-13 15:12:38 +0200

    Added Customers_ProfileController

---
+++ Smart-Garage/Controllers/MVC/Customers_ProfileController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Smart_Garage.Helpers;
+using Smart_Garage.Models.ViewModel;
+using Smart_Garage.Services;
+using Smart_Garage.Services.Contracts;
+
+namespace Smart_Garage.Controllers.MVC
+{
+    public class Customers_ProfileController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IMapper autoMapper;
+        public Customers_ProfileController(IUserService userService, IMapper autoMapper)
+        {
+            this.userService = userService;
+            this.autoMapper = autoMapper;
+        }
+
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Index()
+        {
+            string username = HttpContext.Session.GetString("user");
+            var customerResponseDTO = userService.GetByName(username);
+            var customerViewModel = autoMapper.Map<CustomerViewModel>(customerResponseDTO);
+
+            return View(customerViewModel);
+        }
+    }
+}
