67d946084d98bb8bde46a39b5fafc952f7033f46
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-19 15:18:01 +0200

    Added BrandReponseDTO

---
+++ Smart-Garage/Models/DTOs/ResponseDTOs/BrandResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Smart_Garage.Models.DTOs.ResponseDTOs
+{
+    public class BrandResponseDTO
+    {
+        public string Name { get; set; }
+        public IList<ModelResponseDTO> Models { get; set; }
+    }
+}
