38ae0944e153f6bca689a1f167f22b97d6d1eb3d
Hristian <hristian.kr.nikolov@gmail.com>
2024-02-19 15:19:57 +0200

    Added BrandViewModel

--- Smart-Garage/Models/QueryParameters/UserQueryParameters.cs
+++ Smart-Garage/Models/QueryParameters/UserQueryParameters.cs
@@ -2,10 +2,17 @@
 {
     public class UserQueryParameters
     {
+        // USer
         public string? Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
+
+        // Vehicle
         public string? LicensePlate { get; set; }
         public string? VIN { get; set; }
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
     }
 }
