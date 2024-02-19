681b412ad4fee72dbaa3deaba6d23da02cda0bcb
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2024-02-19 15:35:34 +0200

    Added Admin Vehicles and Services MVC Controller

--- Smart-Garage/Controllers/MVC/Admin_VehiclesController.cs
+++ Smart-Garage/Controllers/MVC/Admin_VehiclesController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Smart_Garage.Helpers;
+using Smart_Garage.Models.DTOs.RequestDTOs;
 using Smart_Garage.Models.ViewModel;
 using Smart_Garage.Services;
 using Smart_Garage.Services.Contracts;
@@ -27,6 +28,79 @@ namespace Smart_Garage.Controllers.MVC
             var vehicleViewModels = autoMapper.Map<IList<VehicleViewModel>>(vehicleResponseDTO);
 
             return View(vehicleViewModels);
+        }
+
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Create()
+        {
+            var vehicleViewModel = new CreateVehicleViewModel();
+
+            return View(vehicleViewModel);
+        }
+
+        [HttpPost]
+        [IsAuthenticated]
+        public IActionResult Create(CreateVehicleViewModel vehicleViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(vehicleViewModel);
+
+            VehicleRequestDTO vehicleRequestDTO = new VehicleRequestDTO()
+            {
+                LicensePlate = vehicleViewModel.LicensePlate,
+                VIN = vehicleViewModel.VIN,
+                CreationYear = vehicleViewModel.CreationYear,
+                Model = vehicleViewModel.Model,
+                Brand = vehicleViewModel.Brand
+            };
+            _ = vehicleService.Create(GetUsername(), vehicleRequestDTO);
+
+            return RedirectToAction("Index", "Admin_Vehicles");
+        }
+
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Detail([FromRoute] string id)
+        {
+
+            var vehicleResponseDTO = vehicleService.GetById(int.Parse(id));
+
+            var vehicleViewModel = autoMapper.Map<VehicleViewModel>(vehicleResponseDTO);
+
+            return View(vehicleViewModel);
+        }
+
+        [HttpPost]
+        [IsAuthenticated]
+        public IActionResult ShowDetail(string vehicleId)
+        {
+
+            return RedirectToAction("Detail", "Admin_Vehicles", new { id = vehicleId });
+        }
+
+        [HttpGet]
+        [IsAuthenticated]
+        public IActionResult Delete(int id)
+        {
+            vehicleService.Delete(id);
+
+            return RedirectToAction("Index", "Admin_Vehicles");
+        }
+
+        [HttpPost]
+        [IsAuthenticated]
+        public IActionResult Update(VehicleViewModel vehicleViewModel)
+        {
+            var updateVehicleDTO = autoMapper.Map<VehicleRequestDTO>(vehicleViewModel);
+            vehicleService.Update(vehicleViewModel.Id, updateVehicleDTO);
+
+            return RedirectToAction("Detail", "Admin_Vehicle", new { id = vehicleViewModel.Id });
+        }
+
+        private string GetUsername()
+        {
+            return HttpContext.Session.GetString("user");
         }
     }
 }
